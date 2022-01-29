16d28b594b82ed398a73e927fa53aa996b680232
DevelopedByssony <n_uck@naver.com>
2022-01-29 19:19:02 +0900

    깃허브 저장 테스트

--- 20220128/src/string1/String03P399.java
+++ 20220128/src/string1/String03P399.java
@@ -11,15 +11,4 @@ public class String03P399 {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("문장을 입력해주세요.");
 		String a = scanner.next();
-		// int get = tomato.indexOf("to");
-		System.out.println("문장 내에서 카운팅할 단어를 입력해주세요.");
-		String b = scanner.next();
-		int get = a.indexOf(b);
-		System.out.println(get + "번 인덱스에서" + "'" + b + "'" + "가 검출되었습니다.");
-		int count = 0;
-		for (int i = 0; i < a.length(); i++) {
-			Char banana = a.charAt(i);
-			if(banana == b) {
-				count++;
-	}
-}
+
